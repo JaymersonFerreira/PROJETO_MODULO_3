e7bd3f69c9a4bcdec1b52aa2339bfb2c7c75cbaa
Jaymerson Ferreira <68979342+JaymersonFerreira@users.noreply.github.com>
2023-07-13 18:30:45 -0300

    atualizei os update
    Retirado o telefone e endereço

--- update/update.sql
+++ update/update.sql
@@ -21,175 +21,130 @@ WHERE id = 1;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99024786',
-  endereco = 'Rua dos prazeres',
+  status = 'Inativo'
+WHERE id = 36;
+
+UPDATE estudantes
+SET
+  status = 'Inativo'
+WHERE id = 37;
+
+UPDATE estudantes
+SET
   status = 'Inativo'
 WHERE id = 38;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99524786',
-  endereco = 'Rua Palmeiras',
   status = 'Inativo'
 WHERE id = 39;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99084586',
-  endereco = 'Rua Italia',
-  status = 'Inativo'
-WHERE id = 39;
-
-UPDATE estudantes
-SET
-  telefone = '(11) 99984286',
-  endereco = 'Rua Jaruá',
   status = 'Inativo'
 WHERE id = 40;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 997845686',
-  endereco = 'Rua Alviverde',
   status = 'Inativo'
 WHERE id = 41;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99369886',
-  endereco = 'Rua Marisa',
   status = 'Inativo'
 WHERE id = 42;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99852386',
-  endereco = 'Rua Cuiaba',
   status = 'Inativo'
 WHERE id = 43;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99569786',
-  endereco = 'Rua Sergipe',
   status = 'Inativo'
 WHERE id = 44;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99412586',
-  endereco = 'Rua Ceara',
   status = 'Inativo'
 WHERE id = 45;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99412586',
-  endereco = 'Rua Ceara',
   status = 'Inativo'
 WHERE id = 45;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99564786',
-  endereco = 'Rua Iara',
   status = 'Inativo'
 WHERE id = 46;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99657886',
-  endereco = 'Rua Jose Urbano',
   status = 'Inativo'
 WHERE id = 47;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99023686',
-  endereco = 'Rua Javari',
   status = 'Inativo'
 WHERE id = 48;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99102356',
-  endereco = 'Rua catetos',
   status = 'Inativo'
 WHERE id = 49;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99963686',
-  endereco = 'Rua Portugal',
   status = 'Inativo'
 WHERE id = 50;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 9998656',
-  endereco = 'Rua China',
   status = 'Inativo'
 WHERE id = 51;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99741286',
-  endereco = 'Rua Brasil',
   status = 'Inativo'
 WHERE id = 52;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99964886',
-  endereco = 'Rua maraja',
   status = 'Inativo'
 WHERE id = 53;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99845286',
-  endereco = 'Rua wilson',
   status = 'Inativo'
 WHERE id = 54;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99964886',
-  endereco = 'Rua João Palma',
   status = 'Inativo'
 WHERE id = 55;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99985486',
-  endereco = 'Rua Dr Guião',
   status = 'Inativo'
 WHERE id = 56;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99964886',
-  endereco = 'Rua Alice',
   status = 'Inativo'
 WHERE id = 57;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99964586',
-  endereco = 'Rua São Paulo',
   status = 'Inativo'
 WHERE id = 58;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 9998286',
-  endereco = 'Rua Rio de Janeiro',
   status = 'Inativo'
 WHERE id = 59;
 
 UPDATE estudantes
 SET
-  telefone = '(11) 99978286',
-  endereco = 'Rua Iara',
   status = 'Inativo'
 WHERE id = 60;
