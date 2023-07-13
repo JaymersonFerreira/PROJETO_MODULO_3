689cdcca96f7dcafc68e8b4df5a2903cc8ca1755
Jaymerson Ferreira <68979342+JaymersonFerreira@users.noreply.github.com>
2023-07-13 18:33:27 -0300

    atualizei a criação das tabelas
    adicionado a create table facilitador_turma

--- create/create.sql
+++ create/create.sql
@@ -1,4 +1,4 @@
--- Criação da tabela "turmas"
+-- Criação das tabelas
 CREATE TABLE "turmas" (
   "id" SERIAL PRIMARY KEY,
   "nome" VARCHAR(255),
@@ -6,7 +6,6 @@ CREATE TABLE "turmas" (
   "data_fim" DATE
 );
 
---Criação da tabela "cursos"
 CREATE TABLE "cursos" (
   "id" SERIAL PRIMARY KEY,
   "nome" VARCHAR(255),
@@ -15,7 +14,6 @@ CREATE TABLE "cursos" (
   FOREIGN KEY ("id_turma") REFERENCES "turmas" ("id")
 );
 
--- Criação da tabela "modulos"
 CREATE TABLE "modulos" (
   "id" SERIAL PRIMARY KEY,
   "nome" VARCHAR(255),
@@ -26,7 +24,6 @@ CREATE TABLE "modulos" (
   FOREIGN KEY ("id_curso") REFERENCES "cursos" ("id")
 );
 
--- Criação da tabela "estudantes"
 CREATE TABLE "estudantes" (
   "id" SERIAL PRIMARY KEY,
   "nome" VARCHAR(255),
@@ -40,7 +37,6 @@ CREATE TABLE "estudantes" (
   FOREIGN KEY ("id_turma") REFERENCES "turmas" ("id")
 );
 
--- Criação da tabela "pessoas_facilitadoras"
 CREATE TABLE "pessoas_facilitadoras" (
   "id" SERIAL PRIMARY KEY,
   "nome" VARCHAR(255),
@@ -54,70 +50,63 @@ CREATE TABLE "pessoas_facilitadoras" (
   FOREIGN KEY ("id_turma") REFERENCES "turmas" ("id")
 );
 
---log de inserções de estudantes
-CREATE TABLE estudante_insert_log (
-  id SERIAL PRIMARY KEY,
-  estudante_id INTEGER,
-  data_atualizacao TIMESTAMP,
-  novo_status VARCHAR(20),
-  FOREIGN KEY (estudante_id) REFERENCES estudantes (id)
-);
 
--- Criação da função "estudante_status_trigger"
-CREATE OR REPLACE FUNCTION estudante_status_trigger()
-  RETURNS TRIGGER AS $$
-BEGIN
-  IF NEW.status <> OLD.status THEN
-    INSERT INTO estudante_insert_log (estudante_id, data_atualizacao, novo_status)
-    VALUES (NEW.id, CURRENT_TIMESTAMP, NEW.status);
-  END IF;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
--- Criação do acionador "trigger_estudante_status"
-CREATE TRIGGER trigger_estudante_status
-AFTER UPDATE ON estudantes
-FOR EACH ROW
-EXECUTE FUNCTION estudante_status_trigger();
-
--- Criação da tabela "estudante_update_log" 
+-- Criação da tabela estudante_update_log
 CREATE TABLE estudante_update_log (
-  id SERIAL PRIMARY KEY,
-  estudante_id INTEGER,
-  data_atualizacao TIMESTAMP,
-  coluna_afetada VARCHAR(255),
-  valor_anterior TEXT,
-  valor_atual TEXT,
-  FOREIGN KEY (estudante_id) REFERENCES estudantes (id)
+  id_estudante INTEGER,
+  data_update TIMESTAMP,
+  novo_status VARCHAR(50)
 );
 
--- Criação da função "estudante_update_trigger"
-CREATE OR REPLACE FUNCTION estudante_update_trigger()
-  RETURNS TRIGGER AS $$
-DECLARE
-  nome_coluna TEXT;
-  valor_anterior TEXT;
-  valor_atual TEXT;
+--select * from estudante_update_log;
+
+-- Criação da função e gatilho para atualização do atributo status
+CREATE OR REPLACE FUNCTION estudante_update_status_log()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
 BEGIN
-  IF TG_OP = 'UPDATE' THEN
-    FOR nome_coluna IN SELECT column_name FROM information_schema.columns WHERE table_name = 'estudantes' LOOP
-      valor_anterior := OLD[nome_coluna]::text;
-      valor_atual := NEW[nome_coluna]::text;
-      
-      IF valor_anterior <> valor_atual THEN
-        INSERT INTO estudante_update_log (estudante_id, data_atualizacao, coluna_afetada, valor_anterior, valor_atual)
-        VALUES (NEW.id, CURRENT_TIMESTAMP, nome_coluna, valor_anterior, valor_atual);
-      END IF;
-    END LOOP;
-  END IF;
-  
+  -- Insere um registro na tabela estudante_update_log
+  INSERT INTO estudante_update_log (id_estudante, data_update, novo_status)
+  VALUES (NEW.id, current_timestamp, NEW.status);
+
   RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
--- Criação do acionador "trigger_estudante_update"(gatilho)
-CREATE TRIGGER trigger_estudante_update
-AFTER UPDATE ON estudantes
+CREATE TRIGGER UPDATE_STATUS_LOG_TRIGGER
+BEFORE UPDATE ON estudantes
 FOR EACH ROW
-EXECUTE FUNCTION estudante_update_trigger();
+EXECUTE FUNCTION estudante_update_status_log();
+
+
+CREATE TABLE estudante_insert_log (
+  id_estudante INTEGER,
+  data_insert TIMESTAMP
+);
+
+--select * from estudante_insert_log;
+
+CREATE OR REPLACE FUNCTION estudante_log()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+
+  INSERT INTO estudante_insert_log (id_estudante, data_insert) VALUES (new.id, current_timestamp);
+  RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER INSERT_LOG_TRIGGER
+AFTER INSERT ON estudantes 
+FOR EACH ROW
+EXECUTE PROCEDURE estudante_log();
+
+CREATE TABLE facilitador_turma (
+  id_facilitador INT,
+  id_turma INT,
+  FOREIGN KEY (id_facilitador) REFERENCES pessoas_facilitadoras(id),
+  FOREIGN KEY (id_turma) REFERENCES turmas(id)
+);
+
